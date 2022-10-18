dac716d9381e146f30da8a82e1e1128a9271fa2d
Leonardo <leonarddo.gomees@gmail.com>
2022-10-18 19:55:35 -0300

    CRIAÇÃO DA TABELA PRODUTOS

---
+++ CREATE TABLE [dbo].[TABELA DE PRODUTOS](.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[TABELA DE PRODUTOS](
+	[CODIGO DO PRODUTO] [varchar](20) NULL,
+	[NOME DO PRODUTO] [varchar](150) NULL,
+	[EMBALAGEM] [varchar](50) NULL,
+	[TAMANHO] [varchar](50) NULL,
+	[SABOR] [varchar](50) NULL,
+	[PREÇO DE LISTA] [smallmoney] NULL
