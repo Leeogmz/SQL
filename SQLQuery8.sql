98aae9c3373b65509e79b472b02e4ca3ed86b04a
Leonardo <leonarddo.gomees@gmail.com>
2022-10-19 22:47:30 -0300

    INSERINDO NOVOS DADOS NA TABELA

---
+++ SQLQuery8.sql
@@ -0,0 +1,37 @@
+
+INSERT INTO [TABELA DE PRODUTOS]
+([CODIGO DO PRODUTO],
+ [NOME DO PRODUTO],
+ EMBALAGEM,
+ TAMANHO,
+ SABOR,
+ [PREÇO DE LISTA])
+VALUES
+('1042712','Linha Citros - 700 ml - Limão','Garrafa','700 ml','Limão',4.90),
+('788975','Pedaços de Frutas - 1,5 Litros - Maça','PET','1,5 Litros','Maça',18.01);
+1002767	Videira do Campo - 700 ml - Cereja/Maça	Garrafa	 700 ml 	 Cereja/Maça	8,41
+231776	Festival de Sabores - 700 ml - Açai	Garrafa	 700 ml 	 Açai	13,31
+479745	Clean - 470 ml - Laranja	Garrafa	 470 ml 	 Laranja	3,77
+1051518	Frescor do Verão - 470 ml - Limão	Garrafa	 470 ml 	 Limão	3,30
+1101035	Linha Refrescante - 1 Litro - Morango/Limão	PET	 1 Litro 	 Morango/Limão	9,01
+229900	Pedaços de Frutas - 350 ml - Maça	Lata	 350 ml 	 Maça	4,21
+1086543	Linha Refrescante - 1 Litro - Manga	PET	 1 Litro 	 Manga	11,01
+695594	Festival de Sabores - 1,5 Litros - Açai	PET	 1,5 Litros 	 Açai	28,51
+838819	Clean - 1,5 Litros - Laranja	PET	 1,5 Litros 	 Laranja	12,01
+326779	Linha Refrescante - 1,5 Litros - Manga	PET	 1,5 Litros 	 Manga	16,51
+520380	Pedaços de Frutas - 1 Litro - Maça	PET	 1 Litro 	 Maça	12,01
+1041119	Linha Citros - 700 ml - Lima/Limão	Garrafa	 700 ml 	 Lima/Limão	4,90
+243083	Festival de Sabores - 1,5 Litros - Maracujá	PET	 1,5 Litros 	 Maracujá	10,51
+394479	Sabor da Montanha - 700 ml - Cereja	Garrafa	 700 ml 	 Cereja	8,41
+746596	Light - 1,5 Litros - Melância	PET	 1,5 Litros 	 Melância	19,51
+773912	Clean - 1 Litro - Laranja	PET	 1 Litro 	 Laranja	8,01
+826490	Linha Refrescante - 700 ml - Morango/Limão	Garrafa	 700 ml 	 Morango/Limão	6,31
+723457	Festival de Sabores - 700 ml - Maracujá	Garrafa	 700 ml 	 Maracujá	4,91
+812829	Clean - 350 ml - Laranja	Lata	 350 ml 	 Laranja	2,81
+290478	Videira do Campo - 350 ml - Melância	Lata	 350 ml 	 Melância	4,56
+783663	Sabor da Montanha - 700 ml - Morango	Garrafa	 700 ml 	 Morango	7,71
+235653	Frescor do Verão - 350 ml - Manga	Lata	 350 ml 	 Manga	3,86
+1002334	Linha Citros - 1 Litro - Lima/Limão	PET	 1 Litro 	 Lima/Limão	7,00
+1013793	Videira do Campo - 2 Litros - Cereja/Maça	PET	 2 Litros 	 Cereja/Maça	24,01
+1096818	Linha Refrescante - 700 ml - Manga	Garrafa	 700 ml 	 Manga	7,71
+1022450	Festival de Sabores - 2 Litros - Açai	PET	 2 Litros 	 Açai	38,01
