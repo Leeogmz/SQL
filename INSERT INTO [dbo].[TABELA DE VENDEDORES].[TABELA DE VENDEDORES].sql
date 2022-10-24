f7d9ce6163e13595eeef4ac52bff7e7626cb0ce1
Leonardo <leonarddo.gomees@gmail.com>
2022-10-24 20:30:43 -0300

    Inserindo dados na tabela de vendedores

---
+++ INSERT INTO [dbo].[TABELA DE VENDEDORES].[TABELA DE VENDEDORES].sql
@@ -0,0 +1,13 @@
+INSERT INTO [dbo].[TABELA DE VENDEDORES]
+           ([MATRICULA]
+           ,[NOME]
+           ,[PERCENTUAL COMISSÃO])
+     VALUES
+           ('00231','Stephanie Cunha',0.10), 
+		   ('00230','Joyce Borges',0.09),
+		   ('00232', 'Kalebe Maynard' , 0.11),
+		   ('00233' , 'Andre Almeida' , 0.12),
+		   ('00234' , 'Italo Alves' , 0.13),
+		   ('00235' , 'Marilene Gomes' , 0.14),
+		   ('00236' , 'Josefa Alves' , 0.15),
+		   ('00237', 'Oviedo Teixeira' , 0.20);
